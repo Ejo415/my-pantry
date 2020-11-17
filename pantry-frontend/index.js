6581f21628f55ce9f28af4c5679068313904b6d3
Ejo415 <Ejo7220@gmail.com>
2020-11-17 11:28:44 -0500

    built out fetch request

--- pantry-frontend/index.js
+++ pantry-frontend/index.js
@@ -1,3 +1,17 @@
-function init(){
-    
+function init() {
+    const containerDiv = document.getElementById("container");
+
+    function getKitchens() {
+        fetch('http://localhost:3000/kitchens')
+        .then(function(res){
+            return res.json()
+        })
+        .then(function(data){
+            console.log(data)
+        })
+    }
+
+    getKitchens()
 }
+
+init();
