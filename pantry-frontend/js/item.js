21e252b3677caa6b81f17370d3a3f2210a1d2c20
Ejo415 <Ejo7220@gmail.com>
2020-11-19 15:43:05 -0500

    split item rendering out from pantry file

---
+++ pantry-frontend/js/item.js
@@ -0,0 +1,42 @@
+class Item{
+
+    static all = []
+
+    constructor(pant){
+        this.id = pant.id
+        this.category = pant.category
+        this.item_name = pant.item_name
+        Item.all.push(this)
+    }
+
+    htmlifyItem(){
+        return (` <ul class="list-group list-group-flush">
+         <li class="list-group-item">${this.item_name} - ${this.category}<div class="dropdown">
+             <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+               Add to
+             </button>
+             <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+               <button class="dropdown-item" type="button">TEST</button>
+            
+             </div>
+           </div></li>
+        
+       </ul>`)
+       }
+
+       static htmlifyItem(){
+        return Item.all.map(item=> item.htmlifyItem()).join("")
+      }
+
+      static renderItem(){
+        //clear item side and replace
+        const itemList = document.getElementById("item-container") 
+        itemList.innerHTML = ""
+        itemList.innerHTML = Item.htmlifyItem()
+  
+    }
+
+
+
+
+}
