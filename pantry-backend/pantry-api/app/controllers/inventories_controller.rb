1852e38a48f12bb35c5346d833a59859be53aca7
Ejo415 <Ejo7220@gmail.com>
2020-11-20 14:35:30 -0500

    created create and destroy methods for inventory

--- pantry-backend/pantry-api/app/controllers/inventories_controller.rb
+++ pantry-backend/pantry-api/app/controllers/inventories_controller.rb
@@ -5,4 +5,27 @@ class InventoriesController < ApplicationController
 
         render :json => inventories, include:  [:kitchen, :item], except: [:created_at, :updated_at, :quantity]
     end
+
+    def create 
+        @inventories = Inventory.new(inv_params)
+        if inv.save 
+            render :json => @inventories, include:  [:kitchen, :item], except: [:created_at, :updated_at, :quantity]
+        end
+    end
+
+    def destroy
+        @inv.destroy
+    end
+
+
+
+
+
+
+
+    private 
+
+    def inv_params
+        params.require(:inventory).permit(:kitchen_id, :item_id)
+    end
 end
