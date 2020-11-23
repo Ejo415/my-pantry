64cd9d8a96fea701839ed36a01cf09758b9d2915
Ejo415 <Ejo7220@gmail.com>
2020-11-23 15:48:44 -0500

    reordered init and added and invInfo call to ln 50

--- pantry-frontend/js/index.js
+++ pantry-frontend/js/index.js
@@ -3,7 +3,10 @@ console.log("index loaded")
 const api = new ApiService();
 
 function init() {
-    renderLeftSide()
+   invInfo()
+   renderItems()
+   bindEventListeners()
+   renderLeftSide()
     
 }
 
@@ -21,7 +24,6 @@ function bindEventListeners(){
 
     removeInvButton.addEventListener('click', function(e) { 
       if(e.target.classList.contains('removeInvButton')){
-        
         removeInv(e)
       }
     });
@@ -29,86 +31,86 @@ function bindEventListeners(){
     newItemForm.addEventListener('submit', addItem);
 }
 
+ async function invInfo(){
+    const data = await api.getInvInfo()
+    for(inv of data){
+    let test =  new Inventory(inv)
+     //console.log(test)
+    }}
 
 async function renderLeftSide(){
+  console.log("render kitchens")
     Pantry.all = []
     const data = await api.getAllKitchens()
     for(kitchen of data){
     let test =  new Pantry(kitchen)
-      console.log(test)
+    //  console.log(test)
     }
     Pantry.renderKitchens()
-    renderItems()
+    invInfo()
     
   }
 
-  async function invInfo(){
-    const data = await api.getInvInfo()
-    for(inv of data){
-    let test =  new Inventory(inv)
-      console.log(test)
-    }}
-
+ 
 async function renderItems(){
     const data = await api.getAllItems()
-    console.log(data)
+    //console.log(data)
     for(item of data){
     new Item(item)
     }
     Item.renderItem()
-    bindEventListeners()
-    invInfo()
+    
 }
+
 
 
 async function addKitchen(e) {
     e.preventDefault();
     const input = e.target.name.value;
     const kData = {name: input}
-    console.log(kData)
+    //console.log(kData)
     const res = await api.addKitchen(kData);
-    console.log(res)
+    //console.log(res)
     new Pantry(res)
     e.target.reset();
     Pantry.renderKitchens()
  }
 
  async function addItem(e) {
-  e.preventDefault();
-  const input1 = e.target.itemName.value;
-  const input2 = e.target.category.value;
-  const iData = {item_name: input1, category:input2 }
-  console.log(iData)
-  const res = await api.addItem(iData);
-  console.log(res)
-  new Item(res)
-  e.target.reset();
-  Item.renderItem()
+    e.preventDefault();
+    const input1 = e.target.itemName.value;
+    const input2 = e.target.category.value;
+    const iData = {item_name: input1, category:input2 }
+    //console.log(iData)
+    const res = await api.addItem(iData);
+    console.log(res)
+    new Item(res)
+    e.target.reset();
+    Item.renderItem()
 }
 
 async function addInv(e) {
-
-  e.preventDefault();
-  //debugger
-  const input1 = e.target.id;
-  const input2 = e.target.parentElement.id;
-  const invData = {kitchen_id: input1, item_id: input2 }
-  console.log(invData)
-  const res = await api.invAdd(invData);
-  console.log(res)
-  renderLeftSide()
+    e.preventDefault();
+    //debugger
+    const input1 = e.target.id;
+    const input2 = e.target.parentElement.id;
+    const invData = {kitchen_id: input1, item_id: input2 }
+   // console.log(invData)
+    const res = await api.invAdd(invData);
+    //console.log(res)
+    renderLeftSide()
+    
 }
 
 async function removeInv(e) {
-  console.log(e.target)
+ // console.log(e.target)
   e.preventDefault();
- 
   const kit = e.target.parentElement.parentElement.id;
   const i = e.target.id;
   const invData = Inventory.findInv(kit,i) 
   //debugger
   const sendData = {id: invData}
-  console.log(sendData)
+ // console.log(sendData)
   const res = await api.invRemove(sendData);
   //console.log(res)
   renderLeftSide()
